7bd8cfb140cb78fb8176248de965b65babcb3e52
HassenGHR <109977314+HassenGHR@users.noreply.github.com>
2023-11-11 21:36:50 +0100

    styling navbar with module css

--- Components/NavBar/Navbar.jsx
+++ Components/NavBar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "tailwindcss/tailwind.css";
+import styles from "./Navbar.module.css";
 import Link from "next/link";
 
 const Navbar = () => {
@@ -10,43 +10,75 @@ const Navbar = () => {
   };
 
   return (
-    <div className="navBar flex justify-between items-center p-6 md:p-10 lg:p-10 xl:p10">
-      <div className="flex items-center">
-        <div className="logoDiv">
-          <h1 className="logo text-2xl md:text-3xl lg:text-4xl xl:text-5xl text-blueColor">
-            <strong>Job</strong>Search
-          </h1>
-        </div>
+    <div className={styles.navBar}>
+      <div className={styles.logoDiv}>
+        <h1 className={styles.logo}>
+          <strong>Job</strong>Search
+        </h1>
       </div>
-      <div className={`menu md:flex flex-col md:flex-row gap-4 ${isOpen ? 'block' : 'hidden'}`}>
-        <li className="menueList text-[#6f6f6f] hover:text-blueColor">
-          <Link href="/">Jobs</Link>
-        </li>
-        <li className="menueList text-[#6f6f6f] hover:text-blueColor">
-          <Link href="/companies"> Companies</Link>
-        </li>
-        <li className="menueList text-[#6f6f6f] hover:text-blueColor">
-          <Link href="/about"> About</Link>
-        </li>
-        <li className="menueList text-[#6f6f6f] hover:text-blueColor">
-          <Link href="/contact"> Contact</Link>
-        </li>
-        <li className="menueList text-[#6f6f6f] hover:text-blueColor">
-          <Link href="/blog"> Blog</Link>
-        </li>
-        <li className="menueList text-[#6f6f6f] hover:text-blueColor">
-        <Link href="/login"> Login</Link>
+
+      <div className={`${styles.menu} ${isOpen ? styles["menu--open"] : ""}`}>
+        <ul className={styles.menuList}>
+          <li
+            className={`${styles.menuItem} text-[#6f6f6f] hover:text-blueColor`}
+          >
+            <Link href="/">Jobs</Link>
           </li>
-        <li className="menueList text-[#6f6f6f] hover:text-blueColor">
-        <Link href="/register"> Register</Link>
-        </li>
+          <li
+            className={`${styles.menuItem} text-[#6f6f6f] hover:text-blueColor`}
+          >
+            <Link href="/">Companies</Link>
+          </li>
+          <li
+            className={`${styles.menuItem} text-[#6f6f6f] hover:text-blueColor`}
+          >
+            <Link href="/">About</Link>
+          </li>
+          <li
+            className={`${styles.menuItem} text-[#6f6f6f] hover:text-blueColor`}
+          >
+            <Link href="/">Contact</Link>
+          </li>
+          <li
+            className={`${styles.menuItem} text-[#6f6f6f] hover:text-blueColor`}
+          >
+            <Link href="/">Blog</Link>
+          </li>
+          <li
+            className={`${styles.menuItem} text-[#6f6f6f] hover:text-blueColor`}
+          >
+            <Link href="/">Login</Link>
+          </li>
+          <li
+            className={`${styles.menuItem} text-[#6f6f6f] hover:text-blueColor`}
+          >
+            <Link href="/">Register</Link>
+          </li>
+        </ul>
       </div>
-      <div className="menuIcon md:hidden cursor-pointer" onClick={toggleMenu}>
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+
+      <div className={`${styles.menuIcon} md:hidden`} onClick={toggleMenu}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
           {isOpen ? (
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M6 18L18 6M6 6l12 12"
+            />
           ) : (
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16m-7 6h7" />
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M4 6h16M4 12h16m-7 6h7"
+            />
           )}
         </svg>
       </div>
